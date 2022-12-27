efdfeda16bb849932392220e931b5de1a20cd693
Arun Kumar Verma <arun.verma@inmobi.com>
2022-12-27 18:51:20 +0530

    first commit

---
+++ src/main/java/com/example/protobuf_tutorial/ProtobufTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.protobuf_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufTutorialApplication.class, args);
+    }
+
+}
